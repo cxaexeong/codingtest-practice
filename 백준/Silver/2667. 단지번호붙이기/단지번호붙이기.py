28019690d06a367c0278c727ed949ab34057fc8f
cylee <130729886+cxaexeong@users.noreply.github.com>
2024-11-20 11:09:59 +0900

    [Silver I] Title: 단지번호붙이기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -0,0 +1,29 @@
+def bfs(si,sj):
+    q = []
+    q.append((si,sj))
+    v[si][sj] = 1
+    cnt = 1
+    
+    while q:
+        ci, cj = q.pop(0)
+        for di, dj in ((-1,0), (1,0), (0,-1), (0,1)):
+            ni, nj = ci + di, cj+ dj
+            if 0 <= ni < n and 0<= nj < n and v[ni][nj] == 0 and arr[ni][nj]==1:
+                q.append((ni,nj))
+                v[ni][nj] = 1
+                cnt += 1
+    return cnt            
+
+n = int(input())
+arr = [list(map(int, input().strip())) for _ in range(n)]
+
+v = [[0]*n for _ in range(n)]
+ans = []
+
+for i in range(n):
+    for j in range(n):
+        if arr[i][j] == 1 and v[i][j] == 0:
+            ans.append(bfs(i,j))
+
+ans.sort()
+print(len(ans), *ans, sep='\n')
