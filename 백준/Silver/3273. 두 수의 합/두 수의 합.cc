e0ded4d93351c1bbae69bee580822b2889e03232
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2024-12-30 12:26:58 +0900

    [Silver III] Title: 두 수의 합, Time: 36 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int n,x,count = 0;
+    cin >> n;
+    vector<int> arr(n);
+    
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    
+    cin >> x;
+    
+    sort(arr.begin(), arr.end());
+    int left = 0, right = n - 1;
+    
+    while (left < right){
+        int sum = arr[left] + arr[right];
+        if (sum == x) {
+            count++;
+            left++;
+            right--;
+        } else if (sum < x){
+            left++;
+        } else if (sum > x){
+            right--;
+        }
+    }
+    
+    cout << count << endl;
+    return 0;
+}
