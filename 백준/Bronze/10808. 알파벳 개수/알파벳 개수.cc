73b8cee12d4f9e367abc5584e8dab49e1d6cd835
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2024-12-30 11:03:17 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+    int counts[26] = {0};
+    
+    for (char c:s){
+        counts[c-'a']++;
+    }
+    
+    for (int i = 0; i < 26; i++){
+        cout << counts[i] << " ";
+    }
+    
+    return 0;
+}
