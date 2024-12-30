fca6429fb99677c58029cafb30c5ee1b5a160792
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2024-12-30 12:28:06 +0900

    Update 두 수의 합.cc

--- 백준/Silver/3273. 두 수의 합/두 수의 합.cc
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -6,7 +6,7 @@ using namespace std;
 int main(){
     int n,x,count = 0;
     cin >> n;
-    vector<int> arr(n);
+    vector<int> arr(n); // arr = [0,0,,,,n]
     
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
