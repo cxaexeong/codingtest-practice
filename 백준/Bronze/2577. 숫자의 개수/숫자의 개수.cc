b73400580521bc02ea4b6ddd4a90955c4ae0e9fc
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-01-02 17:14:06 +0900

    [Bronze II] Title: 숫자의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
@@ -1,24 +1,21 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(){
     int a,b,c;
-    cin >> a>> b>> c;
+    cin >> a >> b >> c;
     
-    int total = a * b * c;
+    int total = a*b*c;
     int arr[10] = {0};
     
     string totalstr = to_string(total);
     
-    for (char digit:totalstr){
-        arr[digit-'0']++;
+    for (char digit : totalstr){
+        arr[digit - '0']  += 1;
     }
     
-    for (int count:arr){
+    for (int count : arr){
         cout << count << endl;
     }
-    
     return 0;
-    
 }
