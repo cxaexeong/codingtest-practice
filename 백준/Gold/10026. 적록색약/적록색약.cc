6f6b1b42c63b17e899741ba8ff7f2da2c27c8838
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-01-07 11:04:36 +0900

    [Gold V] Title: 적록색약, Time: 0 ms, Memory: 2056 KB -BaekjoonHub

---
+++ 백준/Gold/10026. 적록색약/적록색약.cc
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define x first
+#define y second
+
+char board[102][102];
+bool rgx[102][102]; // 일반인 방문 여부
+bool rgo[102][102]; // 적록색약 방문 여부
+int n;
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {1, 0, -1, 0};
+
+// BFS 함수
+void bfs(int x, int y, bool v[102][102], bool is_rg_blind) {
+    queue<pair<int, int>> q;
+    q.push({x, y});
+    v[x][y] = true;
+    char color = board[x][y];
+
+    while (!q.empty()) {
+        auto cur = q.front();
+        q.pop();
+
+        for (int d = 0; d < 4; d++) {
+            int nx = cur.x + dx[d];
+            int ny = cur.y + dy[d];
+
+            if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue; // 범위 체크
+            if (v[nx][ny]) continue; // 이미 방문한 경우 건너뜀
+
+            // 적록색약의 경우 R, G를 동일하게 처리
+            if (is_rg_blind) {
+                if ((color == 'R' || color == 'G') && (board[nx][ny] == 'R' || board[nx][ny] == 'G')) {
+                    v[nx][ny] = true;
+                    q.push({nx, ny});
+                } else if (color == board[nx][ny]) { // 동일 색상일 경우 처리 (추가)
+                    v[nx][ny] = true;
+                    q.push({nx, ny});
+                }
+            } else {
+                // 일반인은 동일한 색상만 이동
+                if (color == board[nx][ny]) {
+                    v[nx][ny] = true;
+                    q.push({nx, ny});
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        string pic;
+        cin >> pic;
+        for (int j = 0; j < n; j++) 
+            board[i][j] = pic[j];
+    }
+
+    // 일반인 경우
+    int rgx_cnt = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (!rgx[i][j]) { // 방문하지 않은 경우만 탐색
+                bfs(i, j, rgx, false);
+                rgx_cnt++;
+            }
+        }
+    }
+
+    // 적록색약인 경우
+    int rgo_cnt = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (!rgo[i][j]) { // 방문하지 않은 경우만 탐색
+                bfs(i, j, rgo, true);
+                rgo_cnt++;
+            }
+        }
+    }
+
+    cout << rgx_cnt << " " << rgo_cnt << '\n';
+    return 0;
+}
