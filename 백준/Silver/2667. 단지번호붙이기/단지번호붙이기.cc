b0e5d86f3867bacc441fd00e1d5efd0843adbeeb
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-01-13 12:51:25 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -3,7 +3,6 @@ using namespace std;
 #define x first
 #define y second
 string board[30];
-//int dist[30];
 bool v[30][30];
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
@@ -12,15 +11,12 @@ int n;
 int bfs(int x, int y) {
     queue<pair<int,int>> q;
     q.push({x, y});
-    v[x][y] = true; 
-    //dist[x][y] = 1; 
+    v[x][y] = true;
     
-    //int count = 0;
-    int count = 1; // 현재 단지의 크기 초기화
+    int count = 1;
     while (!q.empty()) {
         auto [cx, cy] = q.front();
         q.pop();
-        //cnt++;
         
         for (int d = 0; d < 4; d++) {
             int nx = cx + dx[d];
@@ -29,7 +25,6 @@ int bfs(int x, int y) {
             if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
             if (board[nx][ny] == '0' || v[nx][ny]) continue;
             
-            //dist[nx][ny] = dist[cur.x][cur.y] + 1;
             q.push({nx, ny});
             v[nx][ny] = true;
             count++;
@@ -41,23 +36,22 @@ int bfs(int x, int y) {
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
+
     cin >> n;
-    for (int i = 0; i < n; i++) 
+    
+    for (int i = 0; i < n; i++)
         cin >> board[i];
     
-    //int complex = 0; 각 단지의 크기를 complex로 단일 변수로 다룰 수 없으므로, 벡터를 사용해야 함.
-    vector<int> complexes; // 단지 크기 저장
+    vector<int> complex;
     for (int i = 0; i < n; i++)
-        for(int j = 0; j < n; j++) {
-            if (board[i][j] == '1' && !v[i][j])
-                complexes.push_back(bfs(i,j)); // 새로운 단지 발견 시 bfs 수행
+        for (int j = 0; j < n; j++) {
+            if (board[i][j] == '1' && !v[i][j]) 
+                complex.push_back(bfs(i,j));
         }
-        
-    sort(complexes.begin(), complexes.end());  // 단지 크기 정렬
-    cout << complexes.size() << '\n';           // 총 단지 수 출력
-    for (int size : complexes) 
-        cout << size << '\n';                  // 각 단지 크기 출력
-    
-    return 0;  
+    sort(complex.begin(), complex.end());
+    cout << complex.size() << '\n';
+    for (int size : complex) {
+        cout << size << '\n';
+    }
+    return 0;
 }
