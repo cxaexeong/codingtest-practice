03faeade790d9725595c8be61698a5066901d27b
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-01-13 12:34:23 +0900

    Update 말이 되고픈 원숭이.cc

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.cc
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.cc
@@ -1,4 +1,8 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <tuple>
+
 using namespace std;
 
 struct state {
@@ -9,19 +13,33 @@ int main() {
     int k, w, h;
     cin >> k >> w >> h;
 
+    // board는 w x h 크기의 2D 배열, 0으로 초기화
+    // board = [
+    //[0, 0, 0, 0],
+    //[0, 0, 0, 0],
+    //[0, 0, 0, 0],
+    //[0, 0, 0, 0]
+    //];
     vector<vector<int>> board(h, vector<int>(w));
-    for (int i = 0; i < h; i++)
-        for (int j = 0; j < w; j++) {
+    for (int i = 0; i < h; ++i) {
+        for (int j = 0; j < w; ++j) {
             cin >> board[i][j];
         }
+    }
+
+    // v는 3D 배열로 초기화, h x w x (k+1)
     vector<vector<vector<bool>>> v(h, vector<vector<bool>>(w, vector<bool>(k + 1, false)));
 
-    int dx[] = {1, 0, -1, 0};
-    int dy[] = {0, 1, 0, -1};
+    // 일반 이동 (4방향)
+    int dx[] = {0, 0, 1, -1};
+    int dy[] = {1, -1, 0, 0};
 
-    int hourse_dx[] = {1, 2, 2, 1, -1, -2, -2, -1};
-    int hourse_dy[] = {2, 1, -1, -2, -2, -1, 1, 2};
+    // 말 이동 (8방향)
+    int horse_dx[] = {1, 2, 2, 1, -1, -2, -2, -1};
+    int horse_dy[] = {2, 1, -1, -2, -2, -1, 1, 2};
 
+    // q는 state 구조체를 저장하는 빈 큐로 초기화: q.push({0, 0, 0, 0}); // (x, y, k, dist)
+    // 결과 : q = [{x: 0, y: 0, k: 0, dist: 0}]
     queue<state> q;
     q.push({0, 0, 0, 0});
     v[0][0][0] = true;
@@ -49,8 +67,8 @@ int main() {
         // 말 이동
         if (cur.k < k) {
             for (int i = 0; i < 8; ++i) {
-                int nx = cur.x + hourse_dx[i];
-                int ny = cur.y + hourse_dy[i];
+                int nx = cur.x + horse_dx[i];
+                int ny = cur.y + horse_dy[i];
 
                 if (nx >= 0 && ny >= 0 && nx < h && ny < w && !v[nx][ny][cur.k + 1] && board[nx][ny] == 0) {
                     v[nx][ny][cur.k + 1] = true;
