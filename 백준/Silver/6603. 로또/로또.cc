0a55486f48837c60c2177d7371bf648d15653e94
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-01-23 09:47:43 +0900

    [Silver II] Title: 로또, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.cc
+++ 백준/Silver/6603. 로또/로또.cc
@@ -6,11 +6,11 @@ vector<int> s;
 int arr[6];
 
 void backtracking(int depth, int start) {
-    if (depth == 6) {
+    if (depth==6) {
         for (int i = 0; i < 6; i++) 
-            cout << arr[i] << ' ';
+            cout << arr[i] << ' '; 
         cout << '\n';
-        return;
+        return;        
     }
     
     for (int i = start; i < k; i++) {
@@ -25,15 +25,13 @@ int main() {
     
     while (true) {
         cin >> k;
-        if (k == 0) break;
+        if (k==0) break;
         
         s.clear();
         s.resize(k);
         for (int i = 0; i < k; i++)
             cin >> s[i];
-        
         backtracking(0,0);
-        cout << '\n';            // 테스트케이스 사이에 빈 줄 출력
-        
+        cout << '\n';
     }
 }
