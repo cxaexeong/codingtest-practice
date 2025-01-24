c77c7d5ffaf7c3c4f46847a2c73d0a4b60fd826d
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-01-24 20:56:01 +0900

    [Gold III] Title: 감시, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.cc
+++ 백준/Gold/15683. 감시/감시.cc
@@ -3,23 +3,19 @@ using namespace std;
 #define x first
 #define y second
 
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, 1, 0, -1};
-int n, m;
+int dx[] = {0, 1, 0, -1};
+int dy[] = {1, 0, -1, 0};
 int board1[10][10];
 int board2[10][10];
+int n, m;
 vector<pair<int, int>> cctv;
-
-bool OOB(int a, int b) {
-    return (a < 0 || a >= n || b < 0 || b >= m);
-}
 
 void upd(int x, int y, int dir) {
     dir %= 4;
     while (true) {
         x += dx[dir];
         y += dy[dir];
-        if (OOB(x, y) || board2[x][y] == 6) return;
+        if (x < 0 || x >= n || y < 0 || y >= m || board2[x][y] == 6) return;
         if (board2[x][y] != 0) continue;
         board2[x][y] = 7;
     }
@@ -28,18 +24,15 @@ void upd(int x, int y, int dir) {
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    cin >> n >> m;
-    int mn = 0;
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
+    cin >> n >> m;
+    int mn = n*m;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++) { // 수정: j < m
             cin >> board1[i][j];
             if (board1[i][j] != 0 && board1[i][j] != 6)
                 cctv.push_back({i, j});
-            if (board1[i][j] == 0) mn++;
         }
-    }
-
     for (int tmp = 0; tmp < (1 << (2 * cctv.size())); tmp++) {
         memcpy(board2, board1, sizeof(board1));
         int brute = tmp;
@@ -50,33 +43,28 @@ int main() {
             int x = cctv[i].x;
             int y = cctv[i].y;
 
-            if (board1[x][y] == 1) 
+            if (board1[x][y] == 1)
                 upd(x, y, dir);
-            
             else if (board1[x][y] == 2) {
                 upd(x, y, dir);
                 upd(x, y, dir + 2);
-            }
-            else if (board1[x][y] == 3) {
+            } else if (board1[x][y] == 3) {
                 upd(x, y, dir);
                 upd(x, y, dir + 1);
-            }
-            else if (board1[x][y] == 4) {
+            } else if (board1[x][y] == 4) {
                 upd(x, y, dir);
                 upd(x, y, dir + 1);
                 upd(x, y, dir + 2);
-            }
-            else if (board1[x][y] == 5) {
+            } else if (board1[x][y] == 5) {
                 for (int d = 0; d < 4; d++)
-                    upd(x, y, d);
+                    upd(x, y, dir + d); // 수정: dir + d
             }
         }
-
         int val = 0;
         for (int i = 0; i < n; i++)
-            for (int j = 0; j < m; j++)
-                if (board2[i][j] == 0) val++;
-        mn = min(mn, val);
+            for (int j = 0; j < m; j++) // 수정: j < m
+                if (board2[i][j] == 0) val++; // 수정: board2
+        mn = min(val, mn);
     }
     cout << mn;
 }
