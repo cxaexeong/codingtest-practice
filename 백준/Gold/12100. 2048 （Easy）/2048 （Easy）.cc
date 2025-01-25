1b1197ea41b8c8f0b5ab33aa305e51a8b35aece9
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-01-25 12:30:18 +0900

    [Gold I] Title: 2048 (Easy), Time: 12 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.cc
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.cc
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 #define x first
 #define y second
@@ -14,24 +14,26 @@ void rotate() {
             tmp[i][j] = board2[i][j];
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
-            board2[i][j] = tmp[n-1-j][i];
+            board2[i][j] = tmp[n - 1 - j][i];
 }
 
 void tilt(int dir) {
     while (dir--) rotate();
     for (int i = 0; i < n; i++) {
-        int tilted[22] = {}; // board2[i]를 왼쪽으로 기울인 결과를 저장할 변수
-        int idx = 0;         // tilted 배열에서 어디 삽입해야 하는지 가리키는 변수  
+        int tilted[22] = {}; // board2를 왼쪽으로 기울인 결과를 저장할 배열
+        int idx = 0;         // tilted 배열에서 어디 삽입해야 하는지 가리키는 변수
         for (int j = 0; j < n; j++) {
             if (board2[i][j] == 0) continue;
-            if (tilted[idx] == 0)    // 삽입할 위치가 비어 있을 경우
+            if (tilted[idx] == 0) {
                 tilted[idx] = board2[i][j];
-            else if (tilted[idx] == board2[i][j]) // 같은 값을 갖는 블록이 충돌한 경우
+            } else if (tilted[idx] == board2[i][j]) {
                 tilted[idx++] *= 2;
-            else
+            } else {
                 tilted[++idx] = board2[i][j];
+            }
         }
-        for (int j = 0; j < n ; j++) board2[i][j] = tilted[j]; 
+        for (int j = 0; j < n; j++) 
+            board2[i][j] = tilted[j];
     }
 }
 
@@ -41,16 +43,16 @@ int main() {
     
     cin >> n;
     for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++) 
+        for (int j = 0; j < n; j++)
             cin >> board1[i][j];
     
     int mx = 0;
     for (int tmp = 0; tmp < 1024; tmp++) {
-        for (int i = 0; i < n; i++) 
-            for (int j = 0; j < n; j++) 
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
                 board2[i][j] = board1[i][j];
         int brute = tmp;
-        for (int i = 0; i < 5; i++) {
+        for (int d = 0; d < 5; d++) {
             int dir = brute % 4;
             brute /= 4;
             tilt(dir);
@@ -59,5 +61,5 @@ int main() {
             for (int j = 0; j < n; j++)
                 mx = max(mx, board2[i][j]);
     }
-    cout << mx;  
+    cout << mx;    
 }
