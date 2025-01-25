7d47e0770906b9f814bf191193d4ccab40d1e7fe
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-01-25 13:00:57 +0900

    [Gold III] Title: 스티커 붙이기, Time: 4 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/18808. 스티커 붙이기/스티커 붙이기.cc
+++ 백준/Gold/18808. 스티커 붙이기/스티커 붙이기.cc
@@ -1,43 +1,42 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+#define x first
+#define y second
 
-int n, m, k; // 노트북 크기, 스티커 개수
-int note[40][40]; // 노트북 크기
-int r, c; // 스티커 크기
-int sticker[10][10]; // 스티커 크기
+int note[42][42];
+int sticker[12][12];
+int n,m,k;
+int r,c;
 
 void rotate() {
-    int tmp[10][10];
+    int tmp[12][12];
     for (int i = 0; i < r; i++)
         for (int j = 0; j < c; j++)
             tmp[i][j] = sticker[i][j];
     for (int i = 0; i < c; i++)
         for (int j = 0; j < r; j++)
             sticker[i][j] = tmp[r-1-j][i];
-    swap(r, c);
+    swap(r,c);
 }
 
 bool pastable(int x, int y) {
-    for (int i = 0; i < r; i++) {
+    for (int i = 0; i < r; i++)
         for (int j = 0; j < c; j++) {
-            if (x + i >= n || y + j >= m) return false; // 범위 초과
-            if (note[x+i][y+j] == 1 && sticker[i][j] == 1)
+            if (x + i >= n || y + j >= m) return false;
+            if (note[x+i][y+j] == 1 && sticker[i][j] == 1) 
                 return false;
         }
-    }
-    for (int i = 0; i < r; i++) {
-        for (int j = 0; j < c; j++) {
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++)
             if (sticker[i][j] == 1)
                 note[x+i][y+j] = 1;
-        }
-    }
     return true;
 }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-
+    
     cin >> n >> m >> k;
     while (k--) {
         cin >> r >> c;
@@ -45,8 +44,8 @@ int main() {
             for (int j = 0; j < c; j++)
                 cin >> sticker[i][j];
         
-        for (int rot = 0; rot < 4; rot++) {
-            bool is_paste = false;
+        bool is_paste = false;
+        for (int rotation = 0; rotation < 4; rotation++) {
             for (int x = 0; x <= n - r; x++) {
                 if (is_paste) break;
                 for (int y = 0; y <= m - c; y++) {
@@ -60,7 +59,7 @@ int main() {
             rotate();
         }
     }
-
+    
     int cnt = 0;
     for (int i = 0; i < n; i++)
         for (int j = 0; j < m; j++)
