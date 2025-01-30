4ce82514b7e5d37d18f00220d3c1caa47b748909
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-01-30 17:36:17 +0900

    [Gold V] Title: 로봇 청소기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.cc
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.cc
@@ -3,7 +3,6 @@ using namespace std;
 
 int n,m,r,c,d,ans;
 int board[50][50];
-// 북 동 남 서
 int dx[4] = {-1,0,1,0};
 int dy[4] = {0,1,0,-1};
 
@@ -13,15 +12,15 @@ int main() {
     
     cin >> n >> m;
     cin >> r >> c >> d;
+    
     for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++) 
+        for (int j = 0; j < m; j++)
             cin >> board[i][j];
-    int check = 0;
+    
     while (true) {
-        // 청소하지 않은 빈 칸일 경우 청소
         if (board[r][c] == 0) ++ans;
-        board[r][c] = -1;        // 청소한 칸이라고 표시
-        bool cleaned = false;    // 네 방향 중 청소가 된 곳이 있는지
+        board[r][c] = -1;
+        bool cleaned = false;
         for (int i = 0; i < 4; i++) {
             d = (d+3) % 4;
             if (board[r+dx[d]][c+dy[d]] == 0) {
@@ -32,10 +31,8 @@ int main() {
             }
         }
         if (cleaned) continue;
-        // 뒤가 벽으로 막혀있으면 로봇 청소기 종료
         if (board[r-dx[d]][c-dy[d]] == 1)
             break;
-        // 막혀 있지 않을 경우, 후진
         r -= dx[d];
         c -= dy[d];
     }
