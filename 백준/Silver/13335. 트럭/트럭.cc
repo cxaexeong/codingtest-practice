34ae5755a87bfbdb506a179fc6f6fdaa79edd3a6
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-01-30 20:06:27 +0900

    [Silver I] Title: 트럭, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.cc
+++ 백준/Silver/13335. 트럭/트럭.cc
@@ -4,33 +4,30 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-
-    int n, w, l;
+    
+    int n,w,l;
     cin >> n >> w >> l;
     
     vector<int> weight(n);
-    for (int i = 0; i < n; i++) 
+    for (int i = 0; i < n; i++)
         cin >> weight[i];
-
-    queue<pair<int, int>> q;  // (트럭 무게, 다리 진입 시간)
+    
+    queue<pair<int,int>> q; // (트럭무게, 진입시간)
     int time = 0, current_weight = 0, index = 0;
-
+    
     while (index < n || !q.empty()) {
         // 1. 다리에서 빠져나갈 트럭 확인
         if (q.front().second + w == time) {
-            current_weight -= q.front().first;  // 트럭 무게 제거
+            current_weight -= q.front().first;
             q.pop();
         }
-
         // 2. 새로운 트럭 추가 가능 여부 확인
         if (index < n && current_weight + weight[index] <= l) {
-            q.push({weight[index], time});  // 트럭 진입
-            current_weight += weight[index];  // 현재 다리 하중 증가
-            index++;  // 다음 트럭 진행
+            q.push({weight[index], time});
+            current_weight += weight[index];
+            index++;
         }
-
-        time++;  // 1초 증가
+        time++;
     }
-
-    cout << time << '\n';  // 모든 트럭이 다리를 건너는 데 걸린 시간 출력
+    cout << time << '\n';
 }
