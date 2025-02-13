798ca251a7922749f9d6d0e860718bfd548c6914
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-02-13 20:16:26 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 중위순회, Time: 4 ms, Memory: 396 KB -BaekjoonHub

--- SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.cpp
+++ SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.cpp
@@ -1,44 +1,47 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-vector<int> leftChild, rightChild;
-vector<char> nodeData;
+int n;
+char node[101];  // 각 노드의 문자 저장
+int leftChild[101], rightChild[101];  // 왼쪽, 오른쪽 자식 저장
 
-void inorderTraversal(int nodeIdx) {
-    if (nodeIdx == -1) return;
-    inorderTraversal(leftChild[nodeIdx]);
-    cout << nodeData[nodeIdx];
-    inorderTraversal(rightChild[nodeIdx]);
+void inorder(int cur) {
+    if (cur == 0) return;  // 자식이 없으면 리턴
+    inorder(leftChild[cur]);  // 왼쪽 자식 방문
+    cout << node[cur];  // 현재 노드 출력
+    inorder(rightChild[cur]);  // 오른쪽 자식 방문
 }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
-    for (int tc = 1; tc <= 10; ++tc) {
-        int n;
+
+    for (int tc = 1; tc <= 10; tc++) {
         cin >> n;
-        
-        leftChild.assign(n + 1, -1);
-        rightChild.assign(n + 1, -1);
-        nodeData.assign(n + 1, ' ');
-        
-        for (int i = 1; i <= n; ++i) {
-            int index;
-            char data;
-            cin >> index >> data;
-            
-            nodeData[index] = data;
-            
-            if (2 * index <= n) cin >> leftChild[index];  // 왼쪽 자식
-            if (2 * index + 1 <= n) cin >> rightChild[index]; // 오른쪽 자식
+        memset(leftChild, 0, sizeof(leftChild));
+        memset(rightChild, 0, sizeof(rightChild));
+
+        for (int i = 1; i <= n; i++) {
+            int idx;
+            char val;
+            cin >> idx >> val;
+            node[idx] = val;
+            // 다음 입력 문자가 개행문자(\n)인지 확인
+            if (cin.peek() != '\n') {
+                int l;
+                cin >> l;
+                leftChild[idx] = l;
+            }
+            if (cin.peek() != '\n') {
+                int r;
+                cin >> r;
+                rightChild[idx] = r;
+            }
         }
-        
+
         cout << "#" << tc << " ";
-        inorderTraversal(1);
-        cout << "\n";
+        inorder(1);
+        cout << '\n';
     }
-    
     return 0;
 }
