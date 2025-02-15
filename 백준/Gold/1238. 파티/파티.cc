110f8539ba406a6ca91efdbd20b5e504b03f0540
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-02-15 21:08:59 +0900

    [Gold III] Title: 파티, Time: 0 ms, Memory: 2336 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cc
+++ 백준/Gold/1238. 파티/파티.cc
@@ -1,34 +1,31 @@
-// 파티
 #include <bits/stdc++.h>
 using namespace std;
 
-#define x first
-#define y second
-
-const int INF = INT_MAX;
-
-int n,m,x;
+int n, m, x;
+const int INF = 1e9;
 vector<pair<int,int>> adj[1005], rev_adj[1005];
 
 vector<int> dijkstra(int start, vector<pair<int,int>> graph[]) {
     vector<int> dist(n+1, INF);
     priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
-
+    
     dist[start] = 0;
     pq.push({0,start});
-
+    
     while (!pq.empty()) {
-        auto cur = pq.top(); // first = 현재까지 최단 거리, second = 현재 방문하는 노드
+        int cur_dist, cur_node; // 현재까지 최단거리, 현재까지 방문한 노드
+        tie(cur_dist, cur_node) = pq.top();
         pq.pop();
-
-        // 이미 더 짧은 거리로 방문한 적 있으면 무시 (중복 방문 방지)
-        if (dist[cur.y] < cur.x) continue;
-
-        for (auto nxt : graph[cur.y]) { // nxt.x = weight(가중치), nxt.y = 도착 노드
-            // 현재 최단 거리가 더 값이 크면 짧은 거리롤 갱신
-            if (dist[nxt.y] > cur.x + nxt.x) {
-                dist[nxt.y] = cur.x + nxt.x;    // 최단 거리 갱신
-                pq.push({dist[nxt.y], nxt.y}); 
+        
+        if (dist[cur_node] < cur_dist) continue;
+        
+        for (auto nxt : graph[cur_node]) {
+            int weight = nxt.first;
+            int nxt_node = nxt.second;
+            int new_dist = cur_dist + weight;
+            if (new_dist < dist[nxt_node]) {
+                dist[nxt_node] = new_dist;
+                pq.push({new_dist, nxt_node});
             }
         }
     }
@@ -40,22 +37,22 @@ int main() {
     cin.tie(0);
 
     cin >> n >> m >> x;
-
+    
     for (int i = 0; i < m; i++) {
-        int u, v, w;
+        int u,v,w;
         cin >> u >> v >> w;
         adj[u].push_back({w,v});
         rev_adj[v].push_back({w,u});
     }
-
-    vector<int> toX = dijkstra(x, adj); // 모든 마을 -> x까지 최단거리
-    vector<int> fromX = dijkstra(x, rev_adj);    // x -> 모든 마을까지 최단거리
-
-    int max_time = 0;
+    
+    vector<int> toX = dijkstra(x, adj); 
+    vector<int> fromX = dijkstra(x, rev_adj); 
+    
+    int mx = 0;
     for (int i = 1; i <= n; i++) {
-        max_time = max(max_time, toX[i] + fromX[i]);
+        mx = max(mx, toX[i] + fromX[i]);
     }
-
-    cout << max_time << '\n';
-    return 0;
+    
+    cout << mx << '\n';
+    return 0;   
 }
