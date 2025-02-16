cecd90a0f8f5c8d85a728656a02c74a02628b0e6
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-02-16 13:29:02 +0900

    [Bronze II] Title: 공 바꾸기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.cc
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.cc
@@ -4,24 +4,21 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
-    int n,m;
+
+    int n, m;
     cin >> n >> m;
-    vector<int> basket(n+1);
     
-    for (int i = 1; i <= n; i++)
-        basket[i] = i;
-    
-    for (int i = 0; i < m; i++) {
+    vector<int> basket(n + 1);
+    iota(basket.begin(), basket.end(), 0); // 바구니 번호 초기화
+
+    while (m--) {
         int a, b;
         cin >> a >> b;
-        
-        int tmp = basket[a];
-        basket[a] = basket[b];
-        basket[b] = tmp;
+        swap(basket[a], basket[b]); // swap 함수 사용
     }
-    
-    for (int i = 1; i <= n; i++) 
+
+    for (int i = 1; i <= n; i++)
         cout << basket[i] << " ";
+    
     return 0;
 }
