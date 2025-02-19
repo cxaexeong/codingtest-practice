984896d13d791261aa01bab100a914b6f27d879e
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-02-19 10:14:56 +0900

    [Silver III] Title: 시리얼 번호, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/1431. 시리얼 번호/시리얼 번호.cc
+++ 백준/Silver/1431. 시리얼 번호/시리얼 번호.cc
@@ -2,37 +2,34 @@
 using namespace std;
 
 int n;
-vector<string> arr; // 크기 지정 없이 선언
+vector<string> arr;
 
-// 비교 함수 (커스텀 정렬)
-bool cmd(const string& a, const string& b) {
+bool cmp(const string& a, const string& b) {
     int suma = 0, sumb = 0;
     
-    if (a.size() != b.size()) return a.size() < b.size(); // 1. 길이 비교
+    if (a.size() != b.size()) return a.size() < b.size(); // 1. 길이비교
     
     // 2. 숫자 합 계산
     for (char c : a) if (isdigit(c)) suma += (c - '0');
     for (char c : b) if (isdigit(c)) sumb += (c - '0');
-    
-    if (suma != sumb) return suma < sumb; // 2. 숫자 합 비교
-    return a < b; // 3. 사전순 비교
+    if (suma != sumb) return suma < sumb;
+    return a < b;
 }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-   
     cin >> n;
-    arr.resize(n); // 크기 설정
-
+    arr.resize(n);
+    
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
     
-    sort(arr.begin(), arr.end(), cmd); // 전체 정렬
+    sort(arr.begin(), arr.end(), cmp);
     
     for (const string& a : arr) {
         cout << a << '\n';
     }
-    return 0;
+    return 0; 
 }
