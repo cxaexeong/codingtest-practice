d4d09e9aa06936c90d06ca0a79b1d340a337daef
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-02-22 10:21:37 +0900

    [Silver III] Title: 이친수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long dp[91];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n;
+    cin >> n;
+    
+    // 초기값 설정
+    dp[1] = 1;
+    dp[2] = 1;
+    
+    for (int i = 3; i <= n; i++) {
+        dp[i] = dp[i-1] + dp[i-2];
+    }   
+    
+    cout << dp[n] << '\n';
+    return 0;   
+}
