216a781753bffdea11521d6504485a779e026bb5
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-02-22 11:04:19 +0900

    [Silver I] Title: 정수 삼각형, Time: 8 ms, Memory: 3988 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -18,11 +18,10 @@ int main() {
     }
     dp[1][1] = tri[1][1];
     
-    // dp 테이블 채우기
     for (int i = 2; i <= n; i++) {
         for (int j = 1; j <= i; j++) {
             dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + tri[i][j];
         }
     }
-    cout << *max_element(dp[n]+1, dp[n] + n + 1) << '\n';
+    cout << *max_element(dp[n]+1, dp[n]+n+1) << '\n';
 }
