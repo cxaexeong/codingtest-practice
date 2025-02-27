0533dcb961f3d002658ffc5935560b8e7bee947e
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-02-27 09:12:10 +0900

    [Gold IV] Title: 부분합, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.cc
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -7,23 +7,23 @@ vector<int> arr;
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-
+    
     cin >> n >> s;
     arr.resize(n);
     for (int i = 0; i < n; i++) cin >> arr[i];
 
-    int st = 0, en = 0, sum = 0;
-    int min_len = INT_MAX;  // 최소 길이 저장
+    int sum = 0, st = 0, en = 0;
+    int min_len = INT_MAX;
 
     while (en < n) {
-        sum += arr[en];  // 구간 확장
-        while (sum >= s) {  // S 이상이 되면 최소 길이 갱신
-            min_len = min(min_len, en - st + 1);
-            sum -= arr[st];  // st를 오른쪽으로 이동하여 합 줄이기
+        sum += arr[en];
+        while (sum >= s) {
+            min_len = min(min_len, en - st + 1);  
+            sum -= arr[st];
             st++;
         }
         en++;
     }
-
-    cout << (min_len == INT_MAX ? 0 : min_len);  // 가능한 길이가 없으면 0 출력
+    
+    cout << (min_len == INT_MAX ? 0 : min_len);
 }
