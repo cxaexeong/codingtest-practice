fd1a828d0302cbc6962e7efa86d3af036db3c25f
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-02-27 09:13:30 +0900

    [Gold IV] Title: 부분합, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.cc
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, s;
+int n,s;
 vector<int> arr;
 
 int main() {
@@ -11,19 +11,18 @@ int main() {
     cin >> n >> s;
     arr.resize(n);
     for (int i = 0; i < n; i++) cin >> arr[i];
-
+    
     int sum = 0, st = 0, en = 0;
     int min_len = INT_MAX;
-
+    
     while (en < n) {
         sum += arr[en];
         while (sum >= s) {
-            min_len = min(min_len, en - st + 1);  
+            min_len = min(min_len, en - st + 1);
             sum -= arr[st];
             st++;
         }
         en++;
     }
-    
     cout << (min_len == INT_MAX ? 0 : min_len);
 }
