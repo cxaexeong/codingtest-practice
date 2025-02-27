2a46cf724a5becd5a088989703c670d4b51d08d9
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-02-27 10:44:14 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 76 ms, Memory: 12884 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.cc
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.cc
@@ -1,28 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-unordered_set<string> s;  // 중복 없이 이름 저장
+unordered_set<string> s;
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    int n; 
-    cin >> n;  // 출입 기록 개수 입력
-    
+    int n;
+    cin >> n;
     while (n--) {
         string name, log;
-        cin >> name >> log; // 이름과 로그 상태 입력
-        if (log == "enter") s.insert(name);  // 입장 → 추가
-        else s.erase(name);  // 퇴장 → 제거
+        cin >> name >> log;
+        if (log == "enter") s.insert(name);
+        else s.erase(name);
     }
-
-    // 정렬을 위해 unordered_set → vector 변환
+    
     vector<string> ans(s.begin(), s.end());
-
-    // 사전순 역순(내림차순) 정렬
     sort(ans.begin(), ans.end(), greater<string>());
-
-    // 결과 출력
     for (auto x : ans) cout << x << '\n';
 }
