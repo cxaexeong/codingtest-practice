6e536096ef3265ca6c378a19dac0b6d059351e1e
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-03-01 12:04:26 +0900

    [Silver I] Title: 절댓값 힙, Time: 12 ms, Memory: 2532 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.cc
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.cc
@@ -1,11 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 사용자 정의 비교 함수(절대값 기준 정렬, 같으면 원래 값 비교) 
 struct cmp {
     bool operator()(int a, int b) {
-        if (abs(a) == abs(b)) return a > b; // 절대값이 같으면 원래 값이 작은순
-        return abs(a) > abs(b); // 절대값 기준 정렬
+        if (abs(a) == abs(b)) return a> b; // 절대값이 같으면 원래 값이 작은순
+        return abs(a) > abs(b);
     }
 };
 
@@ -13,15 +12,16 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    int n,x;
+    int n;
     cin >> n;
     
     priority_queue<int, vector<int>, cmp> pq;
-  
+    
     for (int i = 0; i < n; i++) {
+        int x;
         cin >> x;
-        if (x == 0) {
-            if (pq.empty()) cout << "0\n";
+        if (x==0) {
+            if(pq.empty()) cout << "0\n";
             else {
                 cout << pq.top() << '\n';
                 pq.pop();
