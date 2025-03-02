6f68f29df963e5f5549cc8217153aa54d2c32b92
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-03-02 14:32:31 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.cc
+++ 백준/Silver/2606. 바이러스/바이러스.cc
@@ -10,7 +10,6 @@ int main() {
     
     int n, num, u, v;
     cin >> n >> num;
-    
     for (int i = 1; i <= num; i++) {
         cin >> u >> v;
         adj[u].push_back(v);
@@ -21,17 +20,15 @@ int main() {
     queue<int> q;
     q.push(1);
     visited[1] = true;
-    
     while (!q.empty()) {
         int cur = q.front(); q.pop();
         
         for (int nxt : adj[cur]) {
-            if (visited[nxt]) continue;  // ✅ 방문한 경우 건너뛴다.
+            if (visited[nxt]) continue;
             q.push(nxt);
             visited[nxt] = true;
             virus++;
         }
     }
-    
-    cout << virus << '\n';  // ✅ 감염된 컴퓨터 개수 출력
+    cout << virus << '\n';
 }
