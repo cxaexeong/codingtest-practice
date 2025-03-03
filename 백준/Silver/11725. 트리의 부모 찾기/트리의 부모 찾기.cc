be586eec304059fbfe7c20558a39be92b0d8ab28
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-03-03 12:03:39 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 40 ms, Memory: 8060 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
@@ -2,31 +2,41 @@
 using namespace std;
 
 int n;
-int p[100001];
-vector<int> adj[100001];
+vector<int> tree[100001];
+int parent[100001];
 
-void dfs(int cur) {
-    for (int nxt : adj[cur]) {
-        if (p[cur] == nxt) continue;
-        p[nxt] = cur;
-        dfs(nxt);
+void bfs() {
+    queue<int> q;
+    q.push(1);    // 루트 노드부터 시작
+    parent[1] = 0;    // 루트의 부모는 없음 (0으로 처리)
+    
+    while (!q.empty()) {
+        int cur = q.front();
+        q.pop();
+        
+        for (int nxt : tree[cur]) {
+            if (parent[nxt] == 0) {    // 아직 부모가 결정되지 않은 경우
+                parent[nxt] = cur;
+                q.push(nxt);
+            }
+        }
     }
 }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-
+    
     cin >> n;
-    // 트리의 간선 개수 = n-1
-    // 입력으로 n-1개의 간선을 받아야하므로..
     for (int i = 0; i < n-1; i++) {
         int u, v;
         cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
+        tree[u].push_back(v);
+        tree[v].push_back(u);
     }
-    dfs(1);
-    // 루트 노드(1번) 은 출력할 필요없음
-    for (int i = 2; i <= n; i++) cout << p[i] << '\n';
+    bfs();
+    for (int i = 2; i <= n; i++) {
+        cout << parent[i] << '\n';
+    }
+    return 0;
 }
