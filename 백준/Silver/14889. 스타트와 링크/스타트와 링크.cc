1472fbd182062af77df6785c227d4b004012c461
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-03-11 17:28:19 +0900

    [Silver I] Title: 스타트와 링크, Time: 104 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
@@ -1,19 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
+int n, s;
 int board[22][22];
-bool team[22];  // true: 스타트 팀, false: 링크 팀
-int minDiff = INT_MAX;  // 최소 능력치 차이 저장
+bool team[22];    // true: 스타트 팀, false: 링크 팀
+int minDiff = INT_MAX;
 
 void dfs(int idx, int cnt) {
-    if (cnt == n / 2) {  // 팀이 반씩 나뉘었을 때
+    if (cnt == n / 2) {
         vector<int> start, link;
         for (int i = 0; i < n; i++) {
             if (team[i]) start.push_back(i);
             else link.push_back(i);
         }
-
         int startPower = 0, linkPower = 0;
         for (int i = 0; i < n / 2; i++) {
             for (int j = 0; j < n / 2; j++) {
@@ -22,11 +21,9 @@ void dfs(int idx, int cnt) {
                 linkPower += board[link[i]][link[j]];
             }
         }
-
         minDiff = min(minDiff, abs(startPower - linkPower));
         return;
     }
-
     for (int i = idx; i < n; i++) {
         if (!team[i]) {
             team[i] = true;
@@ -39,15 +36,13 @@ void dfs(int idx, int cnt) {
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-
+    
     cin >> n;
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < n; j++) {
             cin >> board[i][j];
+        }
     }
-
-    dfs(0, 0);
-    cout << minDiff << '\n';
-
-    return 0;
+    dfs(0,0);
+    cout << minDiff << '\n';    
 }
