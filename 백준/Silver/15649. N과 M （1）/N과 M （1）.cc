b998e7a334669d2fdc3ee9204e2cb1981c2067c0
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-03-11 10:50:49 +0900

    [Silver III] Title: N과 M (1), Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,19 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m;
+int n,m;
 int arr[10];
 bool isused[10];
 
 void backtracking(int k) {
     if (k == m) {
         for (int i = 0; i < m; i++) 
-            cout << arr[i] << ' '; // arr에 기록해둔 수를 출력
+            cout << arr[i] << ' ';
         cout << '\n';
-        return; // 값을 반환하지 않고 단순히 함수 종료
+        return;
     }
     
-    for (int i = 1; i <= n; i++) { // 1부터 시작해야 함
+    for (int i = 1; i <= n; i++) {
         if (!isused[i]) {
             arr[k] = i;
             isused[i] = true;
@@ -22,7 +22,6 @@ void backtracking(int k) {
         }
     }
 }
-
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
