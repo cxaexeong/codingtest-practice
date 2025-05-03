1942e69cd9b514b085fcbdd04fa713b56bc7be75
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-05-03 21:20:11 +0900

    [level 0] Title: 문자열 섞기, Time: 0.01 ms, Memory: 3.61 MB -BaekjoonHub

---
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string str1, string str2) {
+    string result = "";
+    for (int i = 0; i < str1.length(); i++) {
+        result += str1[i];
+        result += str2[i];
+    }
+    return result;
+}
