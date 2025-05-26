054edf1ce36cfe0fd0416556b2785c283877ee4e
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-05-26 21:44:30 +0900

    [Bronze V] Title: 개수 세기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n, v, cnt = 0;
+    int arr[100];
+    
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    scanf("%d", &v);
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == v) cnt++;
+    }
+    printf("%d", cnt);
+    return 0;
+}
