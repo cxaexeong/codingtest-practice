0360854b28a2b9e8a91e71f67c2ca7f371da2abb
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-06-01 21:33:54 +0900

    [Bronze V] Title: X보다 작은 수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.c
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.c
@@ -2,17 +2,11 @@
 
 int main() {
     int n,x;
-    int arr[10001];
-    
     scanf("%d %d", &n, &x);
     
+    int arr[10001];
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
+        if (arr[i] < x) printf("%d ", arr[i]);
     }
-    
-    for (int i = 0; i < n; i++) {
-        if (arr[i] < x)
-            printf("%d ", arr[i]);
-    }
-    return 0;
 }
