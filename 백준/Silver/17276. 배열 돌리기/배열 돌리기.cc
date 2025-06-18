6f503000697356c3fe693b8336365a01e68e34c7
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-06-18 17:56:31 +0900

    [Silver I] Title: 배열 돌리기, Time: 304 ms, Memory: 3480 KB -BaekjoonHub

--- 백준/Silver/17276. 배열 돌리기/배열 돌리기.cc
+++ 백준/Silver/17276. 배열 돌리기/배열 돌리기.cc
@@ -1,41 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-#define MAX (500 + 50)
+#define MAX (550)
 
 int T, N, D;
 int MAP[MAX][MAX];
 int temp[MAX][MAX];
 
-void input()
-{
+void input() {
 	scanf("%d %d", &N, &D);
 
-	for (int r = 1; r <= N; r++)
-		for (int c = 1; c <= N; c++)
+	//D = (D + 360) % 360;
+
+	for (int r = 1; r <= N; r++) {
+		for (int c = 1; c <= N; c++) {
 			scanf("%d", &MAP[r][c]);
+		}
+	}
 }
 
-void printMap()
-{
-	for (int r = 1; r <= N; r++)
-	{
-		for (int c = 1; c <= N; c++)
+void printMap() {
+	for (int r = 1; r <= N; r++) {
+		for (int c = 1; c <= N; c++) {
 			printf("%d ", MAP[r][c]);
+		}
 		putchar('\n');
 	}
 }
 
-void copyMap(int copy[MAX][MAX], int original[MAX][MAX])
-{
-	for (int r = 1; r <= N; r++)
-		for (int c = 1; c <= N; c++)
+void copyMAP(int copy[MAX][MAX], int original[MAX][MAX]) {
+	for (int r = 1; r <= N; r++) {
+		for (int c = 1; c <= N; c++) {
 			copy[r][c] = original[r][c];
+		}
+	}
 }
 
-void rotate45Clockwise()
-{
-	copyMap(temp, MAP);
+void rotate45() { // 시계방향
+	copyMAP(temp, MAP);
 
+	// temp값을 회전시키면서 MAP에 다시 옮김
 	int arr[MAX] = { 0 };
 	int half = (N + 1) / 2;
 
@@ -52,46 +56,40 @@ void rotate45Clockwise()
 	for (int c = 1; c <= N; c++) MAP[half][c] = arr[c];
 }
 
-void rotate45CounterClockwise()
-{
-	copyMap(temp, MAP);
+void rotateCounter45() { // 반시계방향
+	copyMAP(temp, MAP);
 
 	int arr[MAX] = { 0 };
 	int half = (N + 1) / 2;
-
-	// 행 가운데를 왼쪽 아래 ~ 오른쪽 위로
-	for (int c = 1; c <= N; c++) arr[c] = temp[half][c];
-	for (int i = 1; i <= N; i++) MAP[N - i + 1][i] = arr[i];
-
-	// 왼쪽 위 ~ 오른쪽 아래 대각선을 행 가운데로
 	for (int i = 1; i <= N; i++) arr[i] = temp[i][i];
 	for (int c = 1; c <= N; c++) MAP[half][c] = arr[c];
 
-	// 열 가운데를 왼쪽 위 ~ 오른쪽 아래로
 	for (int r = 1; r <= N; r++) arr[r] = temp[r][half];
 	for (int i = 1; i <= N; i++) MAP[i][i] = arr[i];
 
-	// 오른쪽 위 ~ 왼쪽 아래 대각선을 열 가운데로
 	for (int i = 1; i <= N; i++) arr[i] = temp[i][N - i + 1];
 	for (int r = 1; r <= N; r++) MAP[r][half] = arr[r];
+
+	for (int c = 1; c <= N; c++) arr[c] = temp[half][c];
+	for (int i = 1; i <= N; i++) MAP[N - i + 1][i] = arr[i];
+
 }
 
-int main()
-{
+
+int main() {
 	scanf("%d", &T);
 
-	for (int tc = 1; tc <= T; tc++)
-	{
+	for (int tc = 1; tc <= T; tc++) {
 		input();
 
+		// 회전
 		int count = D / 45;
-
-		// D가 음수인 경우 회전 횟수는 양수로 변경
-		count = count > 0 ? count : -count;
-		for (int c = 0; c < count; c++)
-		{
-			if (D >= 0) rotate45Clockwise();
-			else rotate45CounterClockwise();
+		//for (int c = 0; c < count; c++) rotateClockise();
+		if (count > 0) {
+			for (int c = 0; c < count; c++) rotate45();
+		}
+		else {
+			for (int c = 0; c < -count; c++) rotateCounter45();
 		}
 
 		printMap();
