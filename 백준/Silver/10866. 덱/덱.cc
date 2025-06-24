15749c155af8037a3caac0b16ad7524d492fea56
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-06-24 10:52:56 +0900

    [Silver IV] Title: 덱, Time: 0 ms, Memory: 1196 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.cc
+++ 백준/Silver/10866. 덱/덱.cc
@@ -1,58 +1,55 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <stdio.h>
+#define MAX (10000+100)
+#define OFFSET (MAX / 2)
+
+int N;
+int deque[MAX*2];
+int front, back;
+
+int mystrcmp(const char *a, const char *b) {
+    while (*a && *a == *b) a++, b++;
+    return *a - *b;
+}
+
+void printDeque() {
+    for (int i = front; i < back; i++) {
+        printf("%d ", deque[i]);
+    }
+    putchar('\n');
+}
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
     
-    int n;
-    cin >> n;
+    front = back = OFFSET;    // 가운데부터 시작 (front는 -index가 될 수 있으므로)
     
-    deque<int> dq;
+    scanf("%d", &N);
     
-    while (n--) {
-        string command;
-        cin >> command;
+    for (int i = 0; i < N; i++) {
+        char command[100];
+        scanf("%s", command);
         
-        if (command == "push_front") {
-            int x;
-            cin >> x;
-            dq.push_front(x);
-        } 
-        else if (command == "push_back") {
-            int y;
-            cin >> y;
-            dq.push_back(y);
+        if (mystrcmp(command, "push_front") == 0) {
+            int value;
+            scanf("%d", &value);
+            deque[--front] = value; // 포인터를 감소 시킨 후 거기에 값을 대입
         }
-        else if (command == "pop_front") {
-            if (dq.empty()) cout << -1 << '\n';
-            else {
-                cout << dq.front() << '\n';
-                dq.pop_front();
-            }
+        else if (mystrcmp(command, "push_back") == 0) {
+            int value;
+            scanf("%d", &value);
+            deque[back++] = value;    // 포인터를 증가 시킨 후 거기에 값을 대입
         }
-        else if (command == "pop_back") {
-            if (dq.empty()) cout << -1 << '\n';
-            else {
-                cout << dq.back() << '\n';
-                dq.pop_back();
-            }
-        }
-        else if (command == "size") {
-            cout << dq.size() << '\n';
-        }
-        else if (command == "empty") {
-            if (dq.empty()) cout << 1 << '\n';
-            else cout << 0 << '\n';
-        }
-        else if (command == "front") {
-            if (dq.empty()) cout << -1 << '\n';
-            else cout << dq.front() << '\n';
-        }
-        else if (command == "back") {
-            if (dq.empty()) cout << -1 << '\n';
-            else cout << dq.back() << '\n';
-        }      
+        else if (mystrcmp(command, "pop_front") == 0) 
+            printf("%d\n",(back == front)? -1 : deque[front++]);
+        else if (mystrcmp(command, "pop_back") == 0)
+            printf("%d\n", (back == front)? -1 : deque[--back]);
+        else if (mystrcmp(command, "size") == 0)
+            printf("%d\n", back - front);
+        else if (mystrcmp(command, "empty") == 0)
+            printf("%d\n", (back == front)? 1 : 0);
+        else if (mystrcmp(command, "front") == 0)
+            printf("%d\n", (back == front? -1 : deque[front]));
+        else if (mystrcmp(command, "back") == 0)
+            printf("%d\n", (back == front)? -1 : deque[back-1]);
     }
     return 0;
 }
