38d876d6abfed8cf488ec3c635e4ff0176c9a929
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-06-25 09:42:34 +0900

    [Silver III] Title: 회전하는 큐, Time: 0 ms, Memory: 1164 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.cc
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.cc
@@ -1,48 +1,83 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <stdio.h>
+
+#define MAX (50+5)
+#define MAX_DEQUE (MAX * MAX * 2)
+#define OFFSET (MAX_DEQUE / 2)
+
+int N, M;
+int position[MAX];
+
+int deque[MAX_DEQUE * 2];
+int front, back;
+
+void input() {
+
+	scanf("%d %d", &N, &M);
+
+	for (int i = 0; i < M; i++) scanf("%d", &position[i]);
+
+	// init
+	front = back = OFFSET;
+
+	for (int i = 1; i <= N; i++) deque[back++] = i;
+}
+
+int getLeftCount(int value) {
+
+	int ret = 0;
+	for (int i = front; i < back; i++) {
+		if (deque[i] == value) return ret;
+		
+		ret++;
+
+	}
+	return -1; // error
+}
+
+int getRightCount(int value) {
+	
+	int ret = 0;
+	for (int i = back - 1; i >= front; i--) {
+
+		ret++; // 값을 찾은 후 3번 연산을 통해 앞으로 빼줘야 함. 그래서 먼저 ret++
+
+		if (deque[i] == value) return ret;
+
+	}
+	return -1; // error
+}
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
-    int n,k;
-    cin >> n >> k;
-    
-    deque<int> dq;
-    vector<int> targets(k);
-    
-    for (int i = 1; i <= n; i++) {
-        dq.push_back(i);
-    }
-    
-    for (int i = 0; i < k; i++) {
-        cin >> targets[i];
-    }
-    
-    int totalRotations = 0;
-    
-    for (int target : targets) {
-        int idx = find(dq.begin(), dq.end(), target) - dq.begin();
-        
-        int leftRotations = idx;
-        int rightRotations = dq.size() - idx;
-        
-        totalRotations += min(leftRotations, rightRotations);
-        
-        if (leftRotations <= rightRotations) {
-            while (leftRotations--) {
-                dq.push_back(dq.front());
-                dq.pop_front();
-            }
-        } else {
-            while (rightRotations--) {
-                dq.push_front(dq.back());
-                dq.pop_back();
-            }
-        }
-        
-        dq.pop_front();
-    }
-    cout << totalRotations << endl;
-    return 0;    
+	
+	input();
+
+	int ans = 0;
+
+	for (int i = 0; i < M; i++) {
+		
+		if (deque[front] == position[i]) front++;
+		else {
+			int leftCount = getLeftCount(position[i]);
+			int rightCount = getRightCount(position[i]);
+
+			if (leftCount < rightCount) {
+				for (int k = 0; k < leftCount; k++)
+					deque[back++] = deque[front++];
+				
+				front++;
+
+				ans += leftCount;
+			}
+			else {
+				for (int k = 0; k < rightCount; k++) 
+					deque[--front] = deque[--back]; // 뒤에 있는 원소를 뽑아서 앞으로 옮겨줌
+				
+				front++; // front 뽑고
+
+				ans += rightCount;
+			}
+		}
+	}
+
+	printf("%d\n", ans);
 }
