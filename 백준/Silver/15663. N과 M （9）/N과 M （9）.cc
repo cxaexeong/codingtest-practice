a2e229f4db6a329f73585470a250a7fb7ae87e89
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-06-25 22:18:47 +0900

    [Silver II] Title: N과 M (9), Time: 8 ms, Memory: 1156 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -1,43 +1,72 @@
-// 백트래킹 과정에서 직전에 선택한 숫자와 같은 숫자는 건너뜀
-#include <bits/stdc++.h>
-using namespace std;
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 
-int n, m;
-int arr[10];
-bool v[10];
-vector<int> vec;
+#define MAX (8+8)
 
-void backtracking(int depth) {
-    if (depth == m) {
-        for (int i = 0; i < m; i++) 
-            cout << arr[i] << ' ';
-        cout << '\n';
-        return;        
-    }
-    
-    int last = -1;                     // 중복 방지를 위한 변수
-    for (int i = 0; i < n; i++) {
-        if (!v[i] && vec[i] != last) { // 방문x, 이전 값과 다르면 선택
-            v[i] = true;
-            arr[depth] = vec[i];
-            backtracking(depth + 1);
-            v[i] = false;
-            last = vec[i];            // 마지막 선택한 값 갱신
-        }
-    }
+int N, M;
+int num_of_cases[MAX];
+
+int visit[MAX];
+int numbers[MAX];
+int possible[10000 + 500];
+
+void printCases() {
+
+	for (int i = 0; i < M; i++) {
+		printf("%d ", num_of_cases[i]);
+	}
+	putchar('\n');
+}
+
+void DFS(int depth) {
+
+	if (depth == M) {
+		printCases();
+		return;
+	}
+
+	for (int i = 1; i <= N; i++) {
+		if (visit[i] == possible[numbers[i]]) continue; // possible 만큼 썻으면 넘어감
+		num_of_cases[depth] = numbers[i];
+
+		visit[i]++;
+		DFS(depth + 1);
+		visit[i]--;
+	}
+
 }
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
-    cin >> n >> m;
-    vec.resize(n); // vec 크기 조정
-    
-    for (int i = 0; i < n; i++)
-        cin >> vec[i];
-    
-    sort(vec.begin(), vec.end());
-    backtracking(0);
-    return 0;    
+
+	scanf("%d %d", &N, &M);
+	
+	int count = 1; // numbers index 가리킴
+	for (int i = 1; i <= N; i++) {
+		// 값을 하나씩 받고
+		int value;
+		scanf("%d", &value);
+
+		if (possible[value] == 0) numbers[count++] = value;
+
+		// 받을 때마다 possible 배열을 하나씩 증가시켜줌
+		possible[value]++;
+
+	}
+
+	N = count - 1;
+
+	// 정렬
+	for (int i = 1; i <= N; i++) {
+		for (int k = i + 1; k <= N; k++) {
+			if (numbers[i] > numbers[k]) {
+				int tmp = numbers[i];
+				numbers[i] = numbers[k];
+				numbers[k] = tmp;
+			}
+		}
+	}
+
+	DFS(0);
+
+	return 0;
 }
