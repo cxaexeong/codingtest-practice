12fc077d6111d59b5beaa96cfeeafcce320c8990
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-06-29 14:32:26 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 1132 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -1,57 +1,103 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define x first
-#define y second
-string board[30];
-bool v[30][30];
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, 1, 0, -1};
-int n;
+// boj 2667 단지번호 붙이기
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 
-int bfs(int x, int y) {
-    queue<pair<int,int>> q;
-    q.push({x, y});
-    v[x][y] = true;
-    
-    int count = 1;
-    while (!q.empty()) {
-        auto [cx, cy] = q.front();
-        q.pop();
-        
-        for (int d = 0; d < 4; d++) {
-            int nx = cx + dx[d];
-            int ny = cy + dy[d];
-            
-            if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-            if (board[nx][ny] == '0' || v[nx][ny]) continue;
-            
-            q.push({nx, ny});
-            v[nx][ny] = true;
-            count++;
-        }
-    }
-    return count;
+#define MAX (25+5)
+
+int N;
+
+int MAP[MAX][MAX];
+bool visit[MAX][MAX];
+
+int ans[MAX * MAX];
+
+struct  RC
+{
+	int r;
+	int c;
+};
+
+RC queue[MAX * MAX];
+
+// 상 우 하 좌
+int dr[] = {-1, 0, 1, 0};
+int dc[] = {0, 1, 0, -1};
+
+void input() {
+
+	scanf("%d", &N);
+
+	for (int r = 1; r <= N; r++) {
+		for (int c = 1; c <= N; c++) {
+			scanf("%1d", &MAP[r][c]);
+		}
+	}
+}
+
+int BFS(int r, int c) {
+
+	int rp, wp, count;
+
+	rp = wp = 0;
+
+	queue[wp].r = r;
+	queue[wp++].c = c;
+
+	visit[r][c] = true;
+	count = 1;
+
+	while (rp < wp) {
+
+		RC out = queue[rp++];
+
+		for (int i = 0; i < 4; i++) {
+
+			int nr, nc;
+
+			nr = out.r + dr[i];
+			nc = out.c + dc[i];
+
+			if (MAP[nr][nc] == 0 || visit[nr][nc] == true) continue;
+
+			queue[wp].r = nr;
+			queue[wp++].c = nc;
+
+			visit[nr][nc] = true;
+			count++;
+		}
+	}
+
+	return count; // wp 자체가 이미 카운팅하고 있어서 동일함
 }
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
 
-    cin >> n;
-    
-    for (int i = 0; i < n; i++)
-        cin >> board[i];
-    
-    vector<int> complex;
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++) {
-            if (board[i][j] == '1' && !v[i][j]) 
-                complex.push_back(bfs(i,j));
-        }
-    sort(complex.begin(), complex.end());
-    cout << complex.size() << '\n';
-    for (int size : complex) {
-        cout << size << '\n';
-    }
-    return 0;
+	input();
+
+	// 구현
+	int ansCount = 0;
+	for (int r = 1; r <= N; r++) {
+		for (int c = 1; c <= N; c++) {
+			if (MAP[r][c] == 1 && visit[r][c] == false) {
+				ans[ansCount++] = BFS(r, c);
+			}
+		}
+	}
+	printf("%d\n", ansCount);
+
+	// 단지내 오름차순 출력을 위해 정렬
+	for (int i = 0; i < ansCount - 1; i++) {
+		for (int k = i + 1; k < ansCount; k++) {
+			if (ans[i] > ans[k]) {
+				int tmp = ans[i];
+				ans[i] = ans[k];
+				ans[k] = tmp;
+			}
+		}
+	}
+
+	for (int i = 0; i < ansCount; i++)
+		printf("%d\n", ans[i]);
+
+	return 0;
 }
