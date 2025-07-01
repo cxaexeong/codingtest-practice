beb539eb2bd62b7d662e479e351712477a400200
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-07-01 10:11:33 +0900

    [Silver I] Title: 나이트의 이동, Time: 12 ms, Memory: 2392 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.cc
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.cc
@@ -1,67 +1,92 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define x first
-#define y second
-int dx[] = {-2, -1, 1, 2, 2, 1, -1, -2};
-int dy[] = {1, 2, 2, 1, -1, -2, -2, -1};
-int board[302][302];
-bool v[302][302];
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 
-int bfs(int l, int si, int sj, int ei, int ej) {
-    queue<pair<int,int>> q;
-    q.push({si,sj});
-    v[si][sj] = true;
-    
-    int dist[302][302] = {0}; // 이동 횟수 저장
-    
-    while (!q.empty()) {
-        auto cur = q.front();
-        q.pop();
-        
-        // 목적지에 도달하면 이동 횟수 반환
-        if (cur.x == ei && cur.y == ej)
-            return dist[cur.x][cur.y];
-        
-        for (int d = 0; d < 8; d++) {
-            int nx = cur.x + dx[d];
-            int ny = cur.y + dy[d];
-            
-            if (nx < 0 || nx >= l || ny < 0 || ny >= l) continue;
-            if (v[nx][ny]) continue;
-            
-            q.push({nx,ny});
-            v[nx][ny] = true;
-            dist[nx][ny] = dist[cur.x][cur.y] + 1;
-        }
-    }
-    return -1;
+#define MAX (300+30)
+
+int T, L;
+int sr, sc, er, ec;
+int MAP[MAX][MAX];
+
+//int dr[] = {1, 2, 2, 1, -1, -2, -2, -1};
+//int dc[] = {-2, -1, 1, 2, 2, 1, -1, -2, -2};
+int dr[] = { -2, -1, 1, 2, 2, 1, -1, -2, -2 };
+int dc[] = { 1, 2, 2, 1, -1, -2, -2, -1 };
+
+struct RC
+{
+	int r;
+	int c;
+};
+
+RC queue[MAX * MAX];
+
+void input() {
+
+	scanf("%d %d %d %d %d", &L, &sr, &sc, &er, &ec);
+
+	for (int r = 0; r < L; r++) {
+		for (int c = 0; c < L; c++) {
+			MAP[r][c] = 0;
+		}
+	}
+}
+
+void printMAP() {
+
+	for (int r = 0; r < L; r++) {
+		for (int c = 0; c < L; c++) {
+			printf("%d ", MAP[r][c]);
+		}
+		putchar('\n');
+	}
+}
+
+int bfs(int r, int c) {
+
+	int rp, wp;
+
+	rp = wp = 0;
+
+	queue[wp].r = r;
+	queue[wp++].c = c;
+
+	MAP[r][c] = 1;
+
+	while (rp < wp) {
+
+		RC out = queue[rp++];
+
+		if (out.r == er && out.c == ec) return MAP[out.r][out.c] - 1;
+
+		for (int i = 0; i < 8; i++) {
+			
+			int nr = out.r + dr[i];
+			int nc = out.c + dc[i];
+
+			if (nr < 0 || nc < 0 || nr > L - 1 || nc > L - 1) continue;
+			if (MAP[nr][nc] != 0) continue;
+
+			queue[wp].r = nr;
+			queue[wp++].c = nc;
+
+			MAP[nr][nc] = MAP[out.r][out.c] + 1;
+		}
+	}
+	return -1;
 }
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
-    int t;
-    cin >> t;
-    
-    while (t--) {
-        int l;
-        cin >> l;
-                
-        int si, sj, ei, ej;
-        cin >> si >> sj >> ei >> ej;
-        
-        // 방문 배열 초기화
-        memset(v, false, sizeof(v));
-        
-        // 시작점과 끝점이 같다면 0 출력
-        if (si == ei && sj == ej) {
-            cout << 0 << '\n';
-            continue;
-        }
-        
-        // bfs 호출 및 결과 출력
-        cout << bfs(l, si, sj, ei, ej) << '\n';
-    }
-    return 0;
+
+	scanf("%d", &T);
+
+	for (int tc = 0; tc < T; tc++) {
+
+		input();
+		
+		printf("%d\n", bfs(sr, sc));
+		
+		//printMAP();
+	}
+
+	return 0;
 }
