0f43f3b38fe4f3df573c23412d66b43dddc379cf
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-07-14 17:05:53 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 중위순회, Time: 4 ms, Memory: 396 KB -BaekjoonHub

--- SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.cpp
+++ SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.cpp
@@ -1,47 +1,61 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstring>
+
+#define MAX (100+10)
+
 using namespace std;
 
-int n;
-char node[101];  // 각 노드의 문자 저장
-int leftChild[101], rightChild[101];  // 왼쪽, 오른쪽 자식 저장
+int N;
+char node[MAX];
+int left_node[MAX];
+int right_node[MAX];
 
-void inorder(int cur) {
-    if (cur == 0) return;  // 자식이 없으면 리턴
-    inorder(leftChild[cur]);  // 왼쪽 자식 방문
-    cout << node[cur];  // 현재 노드 출력
-    inorder(rightChild[cur]);  // 오른쪽 자식 방문
+void dfs(int cur) {
+	if (cur == 0) return;
+
+	dfs(left_node[cur]);
+	cout << node[cur];
+	dfs(right_node[cur]);
 }
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+	
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 
-    for (int tc = 1; tc <= 10; tc++) {
-        cin >> n;
-        memset(leftChild, 0, sizeof(leftChild));
-        memset(rightChild, 0, sizeof(rightChild));
+	for (int tc = 1; tc <= 10; tc++)
+	{
+		cin >> N;
+		memset(left_node, 0, sizeof left_node);
+		memset(right_node, 0, sizeof right_node);
 
-        for (int i = 1; i <= n; i++) {
-            int idx;
-            char val;
-            cin >> idx >> val;
-            node[idx] = val;
-            // 다음 입력 문자가 개행문자(\n)인지 확인
-            if (cin.peek() != '\n') {
-                int l;
-                cin >> l;
-                leftChild[idx] = l;
-            }
-            if (cin.peek() != '\n') {
-                int r;
-                cin >> r;
-                rightChild[idx] = r;
-            }
-        }
+		for (int i = 1; i <= N; i++)
+		{
+			int idx;
+			char ch;
+			cin >> idx >> ch;
+			node[idx] = ch;
 
-        cout << "#" << tc << " ";
-        inorder(1);
-        cout << '\n';
-    }
-    return 0;
+			// 남은 입력
+			if (cin.peek() != '\n')
+			{
+				int l;
+				cin >> l;
+				left_node[idx] = l;
+			}
+			if (cin.peek() != '\n')
+			{
+				int r;
+				cin >> r;
+				right_node[idx] = r;
+			}
+
+		}
+
+		cout << "#" << tc << " ";
+		dfs(1);
+		cout << '\n';
+	}
 }
