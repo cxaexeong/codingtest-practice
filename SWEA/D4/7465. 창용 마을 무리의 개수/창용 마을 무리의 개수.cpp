06f9a99e11e1597d585d7e50a990daf7b0cfc683
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-07-15 12:31:26 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 8 ms, Memory: 5,844 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.cpp
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.cpp
@@ -1,65 +1,59 @@
 #include <iostream>
 #include <vector>
-#include <unordered_set> 
-
+#include <set>
 using namespace std;
 
-const int MAX_N = 101;
-int parent[MAX_N], rank_[MAX_N]; // 부모 배열 & 랭크 배열
+vector<int> parent;
 
-// 경로 압축을 적용한 find 함수
-int find(int x) { 
-    if (parent[x] != x) 
-        parent[x] = find(parent[x]); // 경로 압축
-    return parent[x];
+void makeSet(int n) {
+
+	parent.resize(n + 1);
+	for (int i = 1; i <= n; i++)
+		parent[i] = i;
+
 }
 
-// Union By Rank를 적용한 union 함수
-void union_set(int a, int b) {
-    int rootA = find(a);
-    int rootB = find(b);
-    
-    if (rootA != rootB) {
-        if (rank_[rootA] > rank_[rootB]) {
-            parent[rootB] = rootA;
-        } else if (rank_[rootA] < rank_[rootB]) {
-            parent[rootA] = rootB;
-        } else {
-            parent[rootB] = rootA;
-            rank_[rootA]++;
-        }
-    }
+int find(int x) {
+
+	if (parent[x] == x)
+		return x;
+
+	return parent[x] = find(parent[x]);
+}
+
+void unite(int x, int y) {
+
+	int px = find(x);
+	int py = find(y);
+
+	if (px != py)
+		parent[py] = px;
+
 }
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
-    int tc;
-    cin >> tc;
-    
-    for (int t = 1; t <= tc; t++) { 
-        int n, m;
-        cin >> n >> m;
 
-        for (int i = 1; i <= n; i++) {
-            parent[i] = i;
-            rank_[i] = 1;
-        }
+	int t;
+	cin >> t;
 
-        for (int i = 0; i < m; i++) {
-            int a, b;
-            cin >> a >> b;
-            union_set(a, b);
-        }
+	for (int tc = 1; tc <= t; tc++) {
 
-        unordered_set<int> root;
-        for (int i = 1; i <= n; i++) {
-            root.insert(find(i));
-        }
+		int n, m;
+		cin >> n >> m;
+		makeSet(n);
 
-        cout << "#" << t << " " << root.size() << '\n';
-    }
+		for (int i = 1; i <= m; i++) {
+			int a, b;
+			cin >> a >> b;
+			unite(a, b);
+		}
 
-    return 0;
+		// 서로 다른 루트의 개수 = 무리 개수
+		set<int> groups;
+		for (int i = 1; i <= n; i++)
+			groups.insert(find(i));
+		
+		cout << "#" << tc << " " << groups.size() << '\n';
+	}
+
 }
