9f8edacae92a30cec433168395d646700a53d868
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-07-16 10:44:59 +0900

    [D6] Title: 수 만들기, Time: 16 ms, Memory: 6,084 KB -BaekjoonHub

--- SWEA/D6/10806. 수 만들기/수 만들기.cpp
+++ SWEA/D6/10806. 수 만들기/수 만들기.cpp
@@ -1,41 +1,57 @@
 #include <iostream>
+#include <vector>
 #include <queue>
-#define x first
-#define y second
-
+#include <unordered_map>
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(0);
+    ios::sync_with_stdio(false);
     cin.tie(0);
 
-    int tc;
-    cin >> tc;
+    int T;
+    cin >> T;
 
-    for (int t = 1; t <= tc; t++) {
-        int N, K;
+    for (int t = 1; t <= T; t++) {
+        int N;
         cin >> N;
 
-        int Nums[11];
-        for (int i = 0; i < N; i++) {
-            cin >> Nums[i];
-        }
+        vector<int> arr(N);
+        for (int i = 0; i < N; i++)
+            cin >> arr[i];
+
+        int K;
         cin >> K;
 
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        pq.push(make_pair(0, K));
+        // 최소 비용을 저장하는 map (value -> cost)
+        unordered_map<int, int> dist;
 
-        while (pq.top().y) {
-            pair<int, int> cur = pq.top();
+        // min-heap 우선순위 큐: {누적비용, 현재값}
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        pq.push({0, K});
+        dist[K] = 0;
+
+        while (!pq.empty()) {
+            int cost = pq.top().first;
+            int value = pq.top().second;
             pq.pop();
 
-            pq.push(make_pair(cur.x + cur.y, 0));
+            if (value == 0) break; // 도달 완료
+
+            if (dist[value] < cost) continue; // 더 좋은 경로가 이미 존재
+
             for (int i = 0; i < N; i++) {
-                pq.push(make_pair(cur.x + cur.y % Nums[i], cur.y / Nums[i]));
+                int next_value = value / arr[i];
+                int next_cost = cost + (value % arr[i]);
+
+                // 갱신 조건: 더 적은 비용일 경우에만 진행
+                if (dist.find(next_value) == dist.end() || next_cost < dist[next_value]) {
+                    dist[next_value] = next_cost;
+                    pq.push({next_cost, next_value});
+                }
             }
         }
 
-        cout << "#" << t << " " << pq.top().x << "\n";
+        cout << "#" << t << " " << dist[0] << '\n';
     }
 
     return 0;
