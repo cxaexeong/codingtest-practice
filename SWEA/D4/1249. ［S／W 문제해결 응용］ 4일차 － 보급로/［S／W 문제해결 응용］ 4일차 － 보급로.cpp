750b593805188720381d5afeee8d1533982c29ba
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-07-18 10:03:24 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 34 ms, Memory: 6,088 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.cpp
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.cpp
@@ -1,70 +1,74 @@
 #include <iostream>
-#include <queue>
 #include <vector>
-#include <climits>  // INT_MAX 사용
+#include <climits>
+#include <queue>
 
 #define x first
 #define y second
+#define MAX (100+10)
 
 using namespace std;
 
-int n;
-int board[101][101];
-int dist[101][101];  // 최단 경로 비용 저장 배열
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, 1, 0, -1};
+int N;
+int MAP[MAX][MAX];
+int dist[MAX][MAX];
 
-// BFS로 최소 복구 시간 찾기
+int dr[] = { -1, 0, 1, 0 };
+int dc[] = { 0, 1, 0, -1 };
+
 int bfs() {
-    queue<pair<int, int>> q;
-    q.push({0, 0});
-    dist[0][0] = board[0][0]; // 시작점 비용 초기화
 
-    while (!q.empty()) {
-        auto cur = q.front();
-        q.pop();
+	queue<pair<int, int>> q;
+	q.push({ 0,0 });
+	dist[0][0] = MAP[0][0]; 
 
-        for (int d = 0; d < 4; d++) {
-            int nx = cur.x + dx[d];
-            int ny = cur.y + dy[d];
+	while (!q.empty()) {
 
-            if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+		pair<int, int> cur = q.front();
+		q.pop();
 
-            // 새로운 비용 계산
-            int newCost = dist[cur.x][cur.y] + board[nx][ny];
+		for (int d = 0; d < 4; d++) {
 
-            // 더 작은 비용이면 갱신하고 큐에 추가
-            if (newCost < dist[nx][ny]) {
-                dist[nx][ny] = newCost;
-                q.push({nx, ny});
-            }
-        }
-    }
-    return dist[n - 1][n - 1];
+			int nr = cur.x + dr[d];
+			int nc = cur.y + dc[d];
+
+			if (nr < 0 || nc < 0 || nr >= N || nc >= N) continue;
+
+			int newCost = dist[cur.x][cur.y] + MAP[nr][nc];
+
+			if (newCost < dist[nr][nc]) {
+				dist[nr][nc] = newCost;
+				q.push({ nr, nc });
+			}
+		}
+	}
+	return dist[N - 1][N - 1];
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
 
-    int tc;
-    cin >> tc;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 
-    for (int t = 1; t <= tc; t++) {
-        cin >> n;
+	int T;
+	cin >> T;
 
-        // 입력 받기 및 결과 배열 초기화
-        for (int i = 0; i < n; i++) {
-            string row;
-            cin >> row;
-            for (int j = 0; j < n; j++) {
-                board[i][j] = row[j] - '0';  // 숫자로 변환
-                dist[i][j] = INT_MAX;      // 초기화
-            }
-        }
+	for (int tc = 1; tc <= T; tc++) {
+		cin >> N;
 
-        int answer = bfs();
-        cout << "#" << t << " " << answer << '\n';
-    }
-    return 0;
+		for (int i = 0; i < N; i++) {
+			string str;
+			cin >> str;
+
+			for (int j = 0; j < N; j++) {
+				MAP[i][j] = str[j] - '0';
+				dist[i][j] = INT_MAX;
+			}
+		}
+
+		int ans = bfs();
+		cout << "#" << tc << " " << ans << '\n';
+	}
+
+	return 0;
 }
