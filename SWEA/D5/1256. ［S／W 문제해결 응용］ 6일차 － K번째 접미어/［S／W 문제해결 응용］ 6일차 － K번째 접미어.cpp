0a2be269b5a5c0773e3a0b95108b8cc348cf019d
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2025-07-19 21:54:26 +0900

    [D5] Title: [S/W 문제해결 응용] 6일차 - K번째 접미어, Time: 5 ms, Memory: 408 KB -BaekjoonHub

--- SWEA/D5/1256. ［S／W 문제해결 응용］ 6일차 － K번째 접미어/［S／W 문제해결 응용］ 6일차 － K번째 접미어.cpp
+++ SWEA/D5/1256. ［S／W 문제해결 응용］ 6일차 － K번째 접미어/［S／W 문제해결 응용］ 6일차 － K번째 접미어.cpp
@@ -1,64 +1,36 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include <algorithm>
-#include <map>
+
 using namespace std;
 
-map<int, map<char, int>> nxt;  // Trie를 사전순 정렬 가능한 map으로 저장
-map<int, bool> chk;
-int unused = 1;  // Root는 0번 노드
-
-// Trie에 접미사 삽입
-void insert(const string& s) {
-    int cur = 0;  // Root 노드부터 시작
-    for (char c : s) {
-        if (nxt[cur].find(c) == nxt[cur].end()) {
-            nxt[cur][c] = unused++;
-        }
-        cur = nxt[cur][c];
-    }
-    chk[cur] = true;
-}
-
-// 사전순 정렬을 위해 Trie 탐색
-vector<string> suffixes;
-void dfs(int node, string s) {
-    if (chk[node]) suffixes.push_back(s);  // 단어 끝에 도달하면 추가
-    for (auto& p : nxt[node]) {  // map은 자동으로 키 기준 정렬됨
-        dfs(p.second, s + p.first);
-    }
-}
-
 int main() {
+
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int tc;  // 테스트 케이스 개수
-    cin >> tc;
-    for (int t = 1; t <= tc; t++) {
-        nxt.clear();
-        chk.clear();
-        unused = 1;
-        suffixes.clear();
+    int T;
+    cin >> T;
 
-        int k;
-        string s;
-        cin >> k >> s;
+    for (int tc = 1; tc <= T; tc++) {
+        int K;
+        string str;
+        cin >> K >> str;
 
-        // 모든 접미사 Trie에 삽입
-        for (size_t i = 0; i < s.size(); i++) {
-            insert(s.substr(i));
-        }
+        vector<string> suffixes;
 
-        // Trie를 DFS 탐색하여 사전순 정렬된 접미사 찾기
-        dfs(0, "");
-        // 이미 DFS에서 사전순으로 삽입되므로 sort 생략 가능하지만 안전하게 유지
+        // 접미어 생성
+        for (int i = 0; i < str.length(); i++)
+            suffixes.push_back(str.substr(i));
+
+        // 사전 순 정렬
         sort(suffixes.begin(), suffixes.end());
 
-        // K번째 접미사 출력
-        cout << "#" << t << " ";
-        if (k > suffixes.size()) cout << "none\n";
-        else cout << suffixes[k - 1] << '\n';
+        cout << "#" << tc << " ";
+        if (K > suffixes.size()) cout << "none";
+        else cout << suffixes[K - 1];
+        cout << '\n';
     }
     return 0;
 }
